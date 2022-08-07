c941f4d24f946fbb701bdab0d48d5cda87527a1c
bkibza <bkibza@ozon.ru>
2022-08-07 17:04:32 +0400

    тест

--- tools/typo/config.py
+++ tools/typo/config.py
@@ -19,14 +19,44 @@ FONT_FAMILIES = [
 
 # Структура типографики
 TYPOGRAPHY_STRUCTURE = {
+    'headline': {
+        'font': FONT_FAMILIES[0],
+        'styles': {
+            'headline': {
+                'typeface': 'Bold',
+            },
+        },
+        'sizes': {
+            'size_pairs': [500, 600, 700, 800],
+            'font_size': 16,
+            'line_height': 20,
+            'paragraph_spacing': 0,
+            'tracking': ['1.4%', '1%', '0.6%', '0.2%'],
+            'font_size_increment': [0, 2, 4, 4],
+            'line_height_increment': 4,
+            'paragraph_spacing_increment': 1,
+            'text_case': 'none',
+            'text_decoration': 'none',
+            'open_type': []
+        },
+        'dense': {
+            'enabled': False,
+            'line_height_increment': -4,
+            'ignore_sizes': [],
+            'open_type': []
+        },
+        'sparse': {
+            'enabled': False,
+            'line_height_increment': 4,
+            'ignore_sizes': [],
+            'open_type': []
+        },
+    },
     'body': {
         'font': FONT_FAMILIES[0],
         'styles': {
             'body': {
                 'typeface': 'Medium',
-            },
-            'body-accent': {
-                'typeface': 'Bold',
             },
         },
         'sizes': {
@@ -43,13 +73,46 @@ TYPOGRAPHY_STRUCTURE = {
             'open_type': []
         },
         'dense': {
-            'enabled': True,
+            'enabled': False,
             'line_height_increment': -4,
             'ignore_sizes': [],
             'open_type': []
         },
         'sparse': {
-            'enabled': True,
+            'enabled': False,
+            'line_height_increment': 4,
+            'ignore_sizes': [],
+            'open_type': []
+        },
+    },
+    'code': {
+        'font': FONT_FAMILIES[1],
+        'styles': {
+            'code': {
+                'typeface': 'Regular',
+            },
+        },
+        'sizes': {
+            'size_pairs': [400, 500],
+            'font_size': 16,
+            'line_height': 24,
+            'paragraph_spacing': 0,
+            'tracking': 0,
+            'font_size_increment': 2,
+            'line_height_increment': 4,
+            'paragraph_spacing_increment': 1,
+            'text_case': 'none',
+            'text_decoration': 'none',
+            'open_type': []
+        },
+        'dense': {
+            'enabled': False,
+            'line_height_increment': -4,
+            'ignore_sizes': [],
+            'open_type': []
+        },
+        'sparse': {
+            'enabled': False,
             'line_height_increment': 4,
             'ignore_sizes': [],
             'open_type': []
