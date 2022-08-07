05a76626b1db4d5bc3e6ee4aedf3d5c6ef3357df
bkibza <bkibza@ozon.ru>
2022-08-07 04:25:46 +0400

    service typography tokens

--- tools/typo/config.py
+++ tools/typo/config.py
@@ -1,3 +1,16 @@
+# НАСТРОЙКИ ТИПОГРАФИКИ
+#
+# Здесь нужно настроить переменные, с помощью которых можно сгенерировать
+# набор токенов для Figma
+#
+# TODO: конвертация токенов под платформы
+# TODO: настройка исключений
+# TODO: варианты для text_decoration и text_case
+# TODO: description к стилям
+
+##############################################################################
+
+
 # Какие шрифты будут использоваться в проекте?
 FONT_FAMILIES = [
     'Manrope',
@@ -6,44 +19,14 @@ FONT_FAMILIES = [
 
 # Структура типографики
 TYPOGRAPHY_STRUCTURE = {
-    'headline': {
-        'font': FONT_FAMILIES[0],
-        'styles': {
-            'headline': {
-                'typeface': 'Bold',
-            },
-        },
-        'sizes': {
-            'size_pairs': [500, 600, 700, 800],
-            'font_size': 16,
-            'line_height': 20,
-            'paragraph_spacing': 0,
-            'tracking': ['1.4%', '1%', '0.6%', '0.2%'],
-            'font_size_increment': [0, 2, 4, 4],
-            'line_height_increment': 4,
-            'paragraph_spacing_increment': 1,
-            'text_case': 'none',
-            'text_decoration': 'none',
-            'open_type': []
-        },
-        'dense': {
-            'enabled': False,
-            'line_height_increment': -4,
-            'ignore_sizes': [],
-            'open_type': []
-        },
-        'sparse': {
-            'enabled': False,
-            'line_height_increment': 4,
-            'ignore_sizes': [],
-            'open_type': []
-        },
-    },
     'body': {
         'font': FONT_FAMILIES[0],
         'styles': {
             'body': {
                 'typeface': 'Medium',
+            },
+            'body-accent': {
+                'typeface': 'Bold',
             },
         },
         'sizes': {
@@ -60,46 +43,13 @@ TYPOGRAPHY_STRUCTURE = {
             'open_type': []
         },
         'dense': {
-            'enabled': False,
+            'enabled': True,
             'line_height_increment': -4,
             'ignore_sizes': [],
             'open_type': []
         },
         'sparse': {
-            'enabled': False,
-            'line_height_increment': 4,
-            'ignore_sizes': [],
-            'open_type': []
-        },
-    },
-    'code': {
-        'font': FONT_FAMILIES[1],
-        'styles': {
-            'code': {
-                'typeface': 'Regular',
-            },
-        },
-        'sizes': {
-            'size_pairs': [400, 500],
-            'font_size': 16,
-            'line_height': 24,
-            'paragraph_spacing': 8,
-            'tracking': 0,
-            'font_size_increment': 2,
-            'line_height_increment': 4,
-            'paragraph_spacing_increment': 1,
-            'text_case': 'none',
-            'text_decoration': 'none',
-            'open_type': []
-        },
-        'dense': {
-            'enabled': False,
-            'line_height_increment': -4,
-            'ignore_sizes': [],
-            'open_type': []
-        },
-        'sparse': {
-            'enabled': False,
+            'enabled': True,
             'line_height_increment': 4,
             'ignore_sizes': [],
             'open_type': []
